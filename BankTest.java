3efaca030ea48e9f2a45840a4a4630f208bd1482
malqattan4 <106008076+malqattan4@users.noreply.github.com>
2022-07-21 14:35:59 +0300

    Update BankTest.java

--- BankTest.java
+++ BankTest.java
@@ -6,31 +6,31 @@ public class BankTest {
         BankAccount acc2 = new BankAccount();
 
         //Account 1 : Deposit into the checking balance and savings balance
-        acc1.DepositCheck(4500);
-        acc1.DepositCheck(500);
-        acc1.DepositSav(1500);
-        acc1.DepositSav(200);
+        acc1.Deposit(1, 4500);
+        acc1.Deposit(1, 500);
+        acc1.Deposit(2, 1500);
+        acc1.Deposit(2, 200);
 
         //Account 1 : Withdraw from checking balance and savings balance
-        acc1.WithdrawCheck(350);
-        acc1.WithdrawSav(50);
-        acc1.WithdrawCheck(200);
-        acc1.WithdrawSav(70);
+        acc1.Withdraw(3, 350);
+        acc1.Withdraw(3, 50);
+        acc1.Withdraw(4, 200);
+        acc1.Withdraw(4, 70);
 
         //Account 1 : Check total amount of the account
         acc1.Total();
 
         //Account 2 : Deposit into the checking balance and savings balance
-        acc2.DepositCheck(500);
-        acc2.DepositCheck(300);
-        acc2.DepositSav(250);
-        acc2.DepositSav(50);
+        acc2.Deposit(1, 500);
+        acc2.Deposit(1, 300);
+        acc2.Deposit(2, 250);
+        acc2.Deposit(2, 50);
 
         //Account 2 : Withdraw from checking balance and savings balance
-        acc2.WithdrawCheck(450);
-        acc2.WithdrawSav(50);
-        acc2.WithdrawCheck(300);
-        acc2.WithdrawSav(70);
+        acc2.Withdraw(3, 450);
+        acc2.Withdraw(3, 50);
+        acc2.Withdraw(4, 300);
+        acc2.Withdraw(4, 70);
 
         //Account 2 : Check total amount of the account
         acc2.Total();
